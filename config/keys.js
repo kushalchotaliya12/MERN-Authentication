6d5cc512d767c0d09128a2ba4a6d1d613d7da4ac
Kushal Chotaliya <kushalv.1997@gmail.com>
2021-01-07 23:09:28 -0500

    Server Setup

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Kushal_123:<password>@cluster0.nhdm1.mongodb.net/UserDB?retryWrites=true&w=majority" 
+  };
+
+  ////mongodb+srv://Kushal_123:Kushal@123@cluster0.nhdm1.mongodb.net/<dbname>?retryWrites=true&w=majority
