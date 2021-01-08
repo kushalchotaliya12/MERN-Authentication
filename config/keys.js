4fa2b7fa49a204017e0324cadf6f0c24142c5b82
Kushal Chotaliya <kushalv.1997@gmail.com>
2021-01-07 23:22:02 -0500

    Deleted keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,3 +3,8 @@ module.exports = {
   };
 
   ////mongodb+srv://Kushal_123:Kushal@123@cluster0.nhdm1.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+  module.exports = {
+    mongoURI: "mongodb+srv://Kushal_123:<password>@cluster0.nhdm1.mongodb.net/UserDB?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
--- routes/api/users.js
+++ routes/api/users.js
@@ -92,3 +92,5 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+module.exports = router;
